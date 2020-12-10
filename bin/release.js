a4d88e118d01f087c29a849b308cfb3c7aa476f2
Marco Koenen <mako@4thmotion.com>
2020-12-10 10:29:48 +0100

    feat: added latest commits

--- bin/release.js
+++ bin/release.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 const log = require('../lib/log')
 const exec = require('../lib/exec')
+const pkgContent = require('../lib/pkg')
+const getTags = require('../lib/tags')
+const getCommits = require('../lib/commits')
 
 const RELEASE_BRANCH = 'develop'
 
@@ -8,9 +11,20 @@ const createRelease = async () => {
   log.empty('Starting the release process…')
 
   // fetch, checkout release branch and pull
-  exec('git fetch origin --quiet', 'Fetch origin.')
-  exec(`git checkout ${RELEASE_BRANCH} --quiet`, `Checkout ${RELEASE_BRANCH} branch.`)
-  exec('git pull origin --quiet', 'Pull from origin.')
+  // exec('git fetch origin --quiet', 'Fetch origin.')
+  // exec(`git checkout ${RELEASE_BRANCH} --quiet`, `Checkout ${RELEASE_BRANCH} branch.`)
+  // exec('git pull origin --quiet', 'Pull from origin.')
+
+  // get the current version and latest tag
+  const pkg = await pkgContent()
+  const version = pkg.version
+  const latestTag = await getTags.latest()
+
+  // commit
+  // get all commits since the last tag and format the output
+  const commits = await getCommits(latestTag.version)
+
+  console.log(commits)
 }
 
 // here we go
